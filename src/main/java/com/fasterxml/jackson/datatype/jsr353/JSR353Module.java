c976299be4ae1bf073a683242492264ac99db92a
Tatu Saloranta <tsaloranta@gmail.com>
2013-02-25 20:12:50 -0800

    ...

--- src/main/java/com/fasterxml/jackson/datatype/jsr353/JSR353Module.java
+++ src/main/java/com/fasterxml/jackson/datatype/jsr353/JSR353Module.java
@@ -1,5 +1,12 @@
 package com.fasterxml.jackson.datatype.jsr353;
 
+import java.util.Collections;
+
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObjectBuilder;
+import javax.json.spi.JsonProvider;
+
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -11,6 +18,12 @@ public class JSR353Module extends SimpleModule
     {
         super(Version.unknownVersion()); //ModuleVersion.instance.version());
 
+        JsonProvider jp = JsonProvider.provider();
+        JsonBuilderFactory builderFactory = jp.createBuilderFactory(Collections.<String,Object>emptyMap());
+
+        JsonArrayBuilder arrayBuilder = builderFactory.createArrayBuilder();
+        JsonObjectBuilder objectBuilder = builderFactory.createObjectBuilder();
+        
         // first deserializers
 //        addDeserializer(DateMidnight.class, new DateMidnightDeserializer());
 
